a0c9972bbfd23eebe7593ca895ef57ad3735b54d
404kacper <kacper.sionkowski@gmail.com>
2023-06-16 13:50:52 +0200

    Removed old toggler from navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -97,18 +97,6 @@ const Navbar = ({
         <Link className='navbar-brand' to='/'>
           <i className='fas fa-door-open'></i> DoorManager
         </Link>
-        {/* Toggler for responsive navbar */}
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
 
         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav ms-auto'>
